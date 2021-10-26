93ae006973fa2a89fd171c0a1a2836665e5709ea
YiJohnZhang <yijohnzhang@gmail.com>
2021-10-25 21:34:43 -0700

    07.01.12A
    07.01.12A

--- 07.01.12_testing/07.01.12A_calculator/calculator-test.js
+++ 07.01.12_testing/07.01.12A_calculator/calculator-test.js
@@ -10,4 +10,14 @@ it("should return a result with 2 decimal places", function() {
   expect(`${calculateMonthlyPayment({amount:5000,years:1,rate:5})}`.split('.')[1].length).toEqual(2);
 });
 
+it('getCurrentUIValues: should return the values of the inputs as is', function(){
+  domReady();
+  expect(parseInt(document.getElementById('loan-amount').value)).toEqual(5000);
+  expect(parseInt(document.getElementById('loan-years').value)).toEqual(1);
+  expect(parseInt(document.getElementById('loan-rate').value)).toEqual(5);
+});
+
+function domReady(){}
+
+document.addEventListener('DOMContentLoaded',domReady);
 /// etc
